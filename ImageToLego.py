28cd7d8cf40404e27ce7bcb932d22caa0387b594
Jack Schultz <schultzjack95@gmail.com>
2023-09-22 00:19:57 -0400

    Small modifications, and prep for hiding secrets

--- ImageToLego.py
+++ ImageToLego.py
@@ -4,12 +4,14 @@ import json
 # init Rebrick tool
 rb = rebrick.Rebrick(api_key="15b882fd571ffae85bdd8cf05d7ab057")
 
-# get set info
-data = rb.get_set(6608)
+# part id 3005 is the basic 1x1 brick
+part_id = 3005
+
+data = rb.get_part(part_id)
 print(data)
 
-response = rb.get_colors()
+response = rb.get_part_colors(part_id)
 
-print("Get colors:")
+print("Total colors:", len(response))
 for i, color in enumerate(response):
-    print(color, type(color))
+    print(i, color)
