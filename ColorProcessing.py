49e3d052f5f85b2d10221e04a952e763e295aecd
Jack Schultz <schultzjack95@gmail.com>
2024-02-13 02:46:33 -0500

    Created color caching functionality. Finished ColorProcessing.

--- ColorProcessing.py
+++ ColorProcessing.py
@@ -1,4 +1,66 @@
-from Pillow import Image
+from PIL import Image
+from math import sqrt
 
-def convertToLegoColors(image, color_list):
-    pass
+def convertImageToLegoColorsTest():
+    '''
+    Use sample image and very basic color list as PoC.
+    '''
+    image = "./eevees.png"
+    color_dict = {
+            "black" : (0,  0, 0),
+            "white" : (255, 255, 255),
+            "red" : (255, 0, 0),
+            "green" : (0, 255, 0),
+            "blue" : (0, 0, 255),
+            "cyan" : (0, 255, 255),
+            "yellow" : (255, 255, 0),
+            "magenta" : (255, 0, 255)
+            }
+
+    with Image.open(image) as im:
+        img = im.load()
+    
+    result_im = im.copy()
+    print(img[im.width // 2, im.height // 2])
+    #img.show()
+    for x in range(im.width):
+        for y in range(im.height):
+            result_im.putpixel((x, y), identifyNewPixelColor(img[x, y], color_dict))
+
+    result_im.show()
+
+    return result_im
+
+
+def convertImageToLegoColors(image, colors):
+    with Image.open(image) as im:
+        px = im.load()
+    result = im.copy()
+
+    for x in range(im.width):
+        for y in range(im.height):
+            result.putpixel((x, y), identifyNewPixelColor(im.getpixel((x, y)), colors))
+
+    result.show()
+    return result
+
+def identifyNewPixelColor(old_color, color_dict):
+    '''
+    Takes the original pixel color and chooses the closest from the provided list of options.
+    Returns a 3-tuple containing the rgb of the selected color.
+    '''
+    (r1, g1, b1) = old_color
+    minimum_distance = float("inf")
+    closest_match = (0, 0, 0)
+
+    for key, value in color_dict.items():
+        (r2, g2, b2) = value
+        distance = sqrt((r2-r1)**2 + (g2-g1)**2 + (b2-b1)**2)
+        if distance < minimum_distance:
+            minimum_distance = distance
+            closest_match = (r2, g2, b2)
+
+    return closest_match
+
+if __name__ == "__main__":
+    convertImageToLegoColorsTest()
--- ImageToLego.py
+++ ImageToLego.py
@@ -1,18 +1,65 @@
 import rebrick
 import json
+from ImageSelector import acquireImage
+import ColorProcessing
 from key import API_KEY
+import options
 
-# init Rebrick tool
-rb = rebrick.Rebrick(api_key=API_KEY)
+def hex_to_rgb(value):
+    lv = len(value)
+    return tuple(int(value[i:i+lv//3], 16) for i in range(0, lv, lv // 3))
 
-# part id 3005 is the basic 1x1 brick
-part_id = 3005
+if __name__ == "__main__":
 
-data = rb.get_part(part_id)
-print(data)
+    OK_TO_CONNECT = True
 
-response = rb.get_part_colors(part_id)
+    # Select an image file
+    file_path = acquireImage()
 
-print("Total colors:", len(response))
-for i, color in enumerate(response):
-    print(i, color)
+    print(file_path)
+    print()
+
+
+    if(OK_TO_CONNECT):
+        # init Rebrick tool
+        rb = rebrick.Rebrick(api_key=API_KEY)
+        
+
+        response = None
+        color_dict = None
+        if (options.UPDATE_COLOR_LIST):
+            data = rb.get_part(options.PART_ID)
+            print(data)
+
+            response = rb.get_part_colors(options.PART_ID)
+
+            color_dict = {}
+            for i, color in enumerate(response):
+                color_dict[color.name] = hex_to_rgb(color.rgb)
+        
+            with open("color_cache.json", 'w', encoding='utf-8') as f:
+                json.dump(color_dict, f, ensure_ascii=False, indent=4)
+        
+        else:
+            with open("color_cache.json", 'r') as f:
+                color_dict = json.load(f)
+
+        print("Total colors:", len(color_dict))
+
+        result = ColorProcessing.convertImageToLegoColors(file_path, color_dict)        
+        
+        save = input("Would you like to save this image? Y/N: ")
+        if save == "Y":
+            #save file
+            sindex = file_path.rfind("/")
+            dindex = file_path.find(".")
+            
+            result.save("output/" + file_path[sindex+1:dindex] + "_conv" + file_path[dindex:])
+            print("File saved in 'output'.")
+        else:
+            #don't save
+            print("File not saved")
+            
+    else:
+        print("Not currently using Rebrick tool.")
+
